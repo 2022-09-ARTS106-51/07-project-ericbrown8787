29dbc108d725cf06cbd1b891db21b6c108e89552
Eric Brown <90017825+ericbrown8787@users.noreply.github.com>
2022-12-15 14:29:10 -0500

    Fix item interaction script
    Script now correctly updates UI element to toggle message display

--- Assets/Scripts/ItemInteractable.cs
+++ Assets/Scripts/ItemInteractable.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ItemInteractable : MonoBehaviour
-{
+{   
+
     public TextMeshProUGUI uiText;
     public string message;
+    public AudioSource source;
+    public AudioClip clip;
     public void Interact() {
         uiText.SetText(message);
+        source.PlayOneShot(clip, 1f);
     }
 }
--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -9,25 +9,31 @@ public class PlayerInteract : MonoBehaviour
 
 
     public TextMeshProUGUI uiText;
-    public void ClearText()
-    {
-        uiText.SetText("");
-    }
+
     void Update()
     {
 
         if (Input.GetMouseButtonDown(0))
         {
-               ClearText();
-            float interactRange = 2f;
+
+            if (uiText.text.Length > 0) { 
+            uiText.SetText("");
+            }
+            else { 
+
+            float interactRange = 1f;
             Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
             foreach (Collider collider in colliderArray)
             {
                 if (collider.TryGetComponent(out ItemInteractable npcInteractable))
                 {
-                    npcInteractable.Interact();
-                }
+             
+                        npcInteractable.Interact();
+                        
+                    }
             }
+            }
+            uiText.ForceMeshUpdate();
         }
     }
 }
